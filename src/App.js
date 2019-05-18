6c03d6e21ac9b63c3d4ac71ce011312c35bca69a
Volha Yasinskaya <vyasinskaya@slb.com>
2019-05-18 17:31:54 +0300

    geolocation test

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import "./App.css";
 import Header from "./Containers/Header";
 import Main from "./Containers/Main";
 import Footer from "./Containers/Footer";
+import Geolocation from "./Components/Geolocation/Geolocation";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Geolocation />
+      {/* <Header />
       <Main />
-      <Footer />
+      <Footer /> */}
     </div>
   );
 }
