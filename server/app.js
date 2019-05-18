309212e297b030e65c10eaf731f55cd8e06c9e8c
slabkodima90 <slabkodima90@gmail.com>
2019-05-18 23:56:00 +0300

    Added searching by coordinates

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const organizationRouter = require('./controllers/organizations');
+const authRouter = require('./controllers/auth');
 
 const app = express();
 
@@ -20,5 +21,6 @@ app.get('/', (req, res) => {
 });
 
 app.use(organizationRouter);
+app.use(authRouter);
 
 module.exports = app;
