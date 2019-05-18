538e9b9efc4abfb093bd87d56f71c84d6e34545e
ikindiBy <ikindi@mail.ru>
2019-05-18 11:46:53 +0300

    feedback/ init

---
+++ src/Components/Footer/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+import "./Footer.css";
+
+class Footer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <footer> site's info | links to social networks </footer>;
+  }
+}
+
+export default Footer;
---
+++ src/Components/Main/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+import "./Main.css";
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <main>Results of search | companie's page | user's page | ... </main>
+    );
+  }
+}
+
+export default Main;
