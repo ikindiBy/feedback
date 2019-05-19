76fcd1ebda57d5a6639ecd5b4870f3fa77e02c9b
slabkodima90 <slabkodima90@gmail.com>
2019-05-19 10:14:14 +0300

    Added rates into sql

--- server/controllers/feedback.js
+++ server/controllers/feedback.js
@@ -5,11 +5,13 @@ const Feedback = {
   addNewFeedbackOnOrgByUser: function (req, res) {
     const params = req.body;
 
-    if (params['org'] && params['user'] && params['overalEstimation'] && params['difficultingProc'] && params['speed'] && params['polite']) {
+    if (params['org'] && params['overalEstimation'] && params['difficultingProc'] && params['speed'] && params['polite']) {
       try {
-        db.any("INSERT INTO public.feedback (user_id, org_id, comment, \"overalEstimation\", \"difficultingProc\", speed, polite, \"hasRecommended\") " +
-          "VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id, date",
-            [params['user'], params['org'], params['comment'], params['overalEstimation'], params['difficultingProc'], params['speed'], params['polite'], params['hasRecommended']])
+        db.any("INSERT INTO public.feedback (user_id, org_id, comment, \"overalEstimation\", \"difficultingProc\", speed, polite, \"hasRecommended\", user_name, email) " +
+          "VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *",
+            [params['user'], params['org'], params['comment'],
+             params['overalEstimation'], params['difficultingProc'],
+             params['speed'], params['polite'], params['hasRecommended'], params['username'], params['email']])
           .then(function (data) {
             res.send(data);
           })
@@ -56,9 +58,10 @@ const Feedback = {
     if (params['org']) {
     const orgId = params['org'];
       try {
-        db.any("select fb.user_id, usr.username, fb.comment, array[fb.\"overalEstimation\", fb.\"difficultingProc\", " +
+        db.any("select fb.user_id, usr.username as logged_user, fb.user_name as quick_fb_username, email, " +
+          "fb.comment, array[fb.\"overalEstimation\", fb.\"difficultingProc\", " +
           "fb.speed, fb.polite] as rates, fb.\"hasRecommended\", " +
-          "fb.date, fb.\"hasResponse\", rfb.response  from public.feedback as fb " +
+          "fb.date, fb.\"hasResponse\", rfb.response, rfb.date as response_date  from public.feedback as fb " +
           "left join public.response_on_feedback as rfb on rfb.feedback_id = fb.id " +
           "left join public.users as usr on usr.id = fb.user_id where fb.org_id = $1", orgId)
           .then(function (data) {
